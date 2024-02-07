cc8f8044886fd1aa4f8069b91bb2e28e8323764b
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-07 14:04:41 -0400

    fixed fereign key in index.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const findAllProducts = await Product.findAll({
+      include: [{ model: Category}, { model: Tag }],
+    })
+    res.status(200).json(findAllProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // get one product
