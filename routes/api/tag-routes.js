51b09c0a0b8f2dd07d67d677c03f1f28ed3dee02
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-07 21:19:25 -0400

    added find one tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,17 @@ router.get('/', async (req, res) => {
   };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const oneTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(oneTag);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.post('/', (req, res) => {
