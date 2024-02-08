4b670a50a448643e8b3793bcb3e2e52be63f1938
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-07 21:22:00 -0400

    added create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,16 @@ router.get('/:id', async (req, res) => {
   };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(201).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.put('/:id', (req, res) => {
